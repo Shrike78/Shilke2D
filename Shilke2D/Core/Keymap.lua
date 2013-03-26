c3ad97e3c970e2bf0a969eb3d0b63b80ffa456fe
luca@vm-alita <luca.ferrara@gmail.com>
2013-03-26 07:50:58 +0100

    rename of special keys for keyboard handling

--- Shilke2D/Core/Keymap.lua
+++ Shilke2D/Core/Keymap.lua
@@ -7,30 +7,30 @@ Moreover some utilities are provided to handle keys
 --]]
 
 ---Special Keys
-Keys = {
-	KEY_DEL = 8,
-	KEY_RETURN = 13,
-	KEY_ESC = 27,
-	KEY_SPACE = 32,
-	KEY_CANC = 127,
-	KEY_F1 = 257,
-	KEY_F2 = 258,
-	KEY_F3 = 259,
-	KEY_F4 = 260,
-	KEY_F5 = 261,
-	KEY_F6 = 262,
-	KEY_F7 = 263,
-	KEY_F8 = 264,
-	KEY_F9 = 265,
-	KEY_F10 = 266,
-	KEY_F11 = 267,
-	KEY_F12 = 268,
-	KEY_LEFT = 356,
-	KEY_UP = 357,
-	KEY_RIGHT = 358,
-	KEY_DOWN = 359,
-	KEY_PAGE_UP = 360,
-	KEY_PAGE_DOWN = 361
+Key = {
+	DEL = 8,
+	RETURN = 13,
+	ESC = 27,
+	SPACE = 32,
+	CANC = 127,
+	F1 = 257,
+	F2 = 258,
+	F3 = 259,
+	F4 = 260,
+	F5 = 261,
+	F6 = 262,
+	F7 = 263,
+	F8 = 264,
+	F9 = 265,
+	F10 = 266,
+	F11 = 267,
+	F12 = 268,
+	LEFT = 356,
+	UP = 357,
+	RIGHT = 358,
+	DOWN = 359,
+	PAGE_UP = 360,
+	PAGE_DOWN = 361
 }
 
 ---Translate a character into a keycode
