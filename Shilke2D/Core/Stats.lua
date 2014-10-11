f3ccf30c8171ab17124649f529d50b6a2462cf5b
Luca Ferrara <luca.ferrara@gmail.com>
2014-10-11 18:04:46 +0200

    Extended Stats informations

--- Shilke2D/Core/Stats.lua
+++ Shilke2D/Core/Stats.lua
@@ -1,16 +1,19 @@
 --[[---
 Stats helper to show on screen framerate, memory occupation, ecc.
+Displayed infos are:
+
+Fps - DrawCalls - Total Memory (lua memory, texture memory)
 --]]
 Stats = class(DisplayObjContainer)
 
 function Stats:init()
 	DisplayObjContainer.init(self)
 	
-	local background = Quad(150,20)
+	local background = Quad(250,20)
 	background:setColor(Color(0,0,0))
 	self:addChild(background)
 	
-	self.info_stats = TextField(150,20, "")
+	self.info_stats = TextField(250,20, "")
 	self.info_stats:setAlignment(MOAITextBox.CENTER_JUSTIFY)
 	self:addChild(self.info_stats)
 end
@@ -22,8 +25,11 @@ Updates with the last frame informations
 function Stats:update()	
 	local fps = math.floor(MOAISim.getPerformance())
 	local usage = MOAISim.getMemoryUsage()
-	--local drawcalls = MOAIRenderMgr.getPerformanceDrawCount()
-	--self.info_stats:setText(string.format("%d - %d - %d", fps, usage.total, drawcalls))
-	self.info_stats:setText(string.format("%d - %d", fps, usage.total))
+	local mtotal = usage.total/(1024*1024)
+	local mlua = usage.lua/(1024*1024)
+	local mtexture = usage.texture/(1024*1024)
+	--keep 1 that's the cost of drawing Stats object itself
+	local drawcalls = MOAIRenderMgr.getPerformanceDrawCount() - 1
+	self.info_stats:setText(string.format("%d - %d - %.3f (%.3f, %.3f)", fps, drawcalls, mtotal, mlua, mtexture))
 end
 
