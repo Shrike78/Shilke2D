6c038c5ea937404ac107ecb064b511847c2fdbaa
Shrike78 <luca.ferrara@gmail.com>
2014-10-21 21:37:02 +0200

    extended_stats

--- Shilke2D/Core/Stats.lua
+++ Shilke2D/Core/Stats.lua
@@ -6,14 +6,19 @@ Fps - DrawCalls - Total Memory (lua memory, texture memory)
 --]]
 Stats = class(DisplayObjContainer)
 
+local extended_stats = false
+
 function Stats:init()
 	DisplayObjContainer.init(self)
 	
-	local background = Quad(300,20)
+	local width = extended_stats and 320 or 300
+	local height = extended_stats and 40 or 20
+	
+	local background = Quad(width,height)
 	background:setColor(Color(0,0,0))
 	self:addChild(background)
 	
-	self.info_stats = TextField(300,20, "")
+	self.info_stats = TextField(width,height, "")
 	self.info_stats:setAlignment(MOAITextBox.CENTER_JUSTIFY)
 	self:addChild(self.info_stats)
 end
@@ -28,7 +33,12 @@ function Stats:update()
 	--minus 1, the cost of drawing Stats object itself
 	local drawcalls = MOAIRenderMgr.getPerformanceDrawCount() - 1
 	
-	self.info_stats:setText(string.format("%d - %d - %.3f (%.3f, %.3f)", fps, drawcalls, 
-			usage.total, usage.lua, usage.texture))
+	if extended_stats then
+		self.info_stats:setText(string.format("%d - %d - %.3f (%.3f, %.3f)\n%.3f, %.3f", fps, drawcalls, 
+				usage.total, usage.lua, usage.texture, usage._sys_rss, usage._sys_vs))
+	else
+		self.info_stats:setText(string.format("%d - %d - %.3f (%.3f, %.3f)", fps, drawcalls, 
+				usage.total, usage.lua, usage.texture))
+	end
 end
 
